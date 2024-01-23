2112e92ead68eba5d7a3e2c8383adc4e37637946
ashok77jayaraman <138790072+ashok77jayaraman@users.noreply.github.com>
2024-01-24 02:27:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def login():
 def dashboard():
     if session.get('logged_in'):
         # Add the image file names you want to display
-        image_files = ['image1.jpg', 'image2.jpg', 'image3.jpg']
+        image_files = ['image1.jpg', 'image2.jpg', 'image3.jpg','dog.jpg']
 
         # Pass the image files to the template
         return render_template('dashboard.html', image_files=image_files)
